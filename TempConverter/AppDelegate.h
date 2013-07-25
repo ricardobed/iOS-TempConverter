512b97647e81b85073871b7da32a5630783de6fe
Ricardo Bedoya <ricardob@yahoo-inc.com>
2013-07-24 23:03:39 -0700

    Initial Commit

---
+++ TempConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TempConverter
+//
+//  Created by Ricardo Bedoya on 7/24/13.
+//  Copyright (c) 2013 Ricardo Bedoya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TempConverterTests/TempConverterTests.m
@@ -0,0 +1,36 @@
+//
+//  TempConverterTests.m
+//  TempConverterTests
+//
+//  Created by Ricardo Bedoya on 7/24/13.
+//  Copyright (c) 2013 Ricardo Bedoya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TempConverterTests : XCTestCase
+
+@end
+
+@implementation TempConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
